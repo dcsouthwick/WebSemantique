9ac46928be72df5932113e9f30b89c26b305a761
David Southwick <david.southwick@cern.ch>
2019-11-06 17:17:48 +0100

    move to gpxpy, rdflib for parsing

--- convert.py
+++ convert.py
@@ -1,12 +1,38 @@
-import untangle
+import rdflib
+import gpxpy
+import gpxpy.gpx
+# https://pypi.org/project/gpxpy/
+# pip install gpxpy rdflib
 from pathlib import Path, PureWindowsPath
 
-data_folder = Path('C:/Users/dcsou/Documents/GPX_Tracks/GPX_Tracks')
-track = data_folder / "4sDDFdd4cjA.gpx"
-f = open(track)
+gpx_file = open('GPX_Tracks/4sDDFdd4cjA.gpx', 'r')
+
+gpx = gpxpy.parse(gpx_file)
+for track in gpx.tracks:
+    for segment in track.segments:
+            for point in segment.points:
+                print('Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation))
+
+for waypoint in gpx.waypoints:
+    print('waypoint {0} -> ({1},{2})'.format(waypoint.name, waypoint.latitude, waypoint.longitude))
+
+for route in gpx.routes:
+    print('Route:')
+    for point in route.points:
+        print('Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation))
+#track = Path.cwd() / "GPX_Tracks" /"4sDDFdd4cjA.gpx"
+#f = open(track)
+#g=rdflib.Graph()
+#semweb=rdflib.URIRef('http://dbpedia.org/resource/Semantic_Web')
+#type=g.value(semweb, rdflib.RDFS.label)
+#g.parse("GPX_Tracks/4sDDFdd4cjA.gpx", format="rdfxml")
+
+#len(g) # prints 2
+
+#import pprint
+#for stmt in g:
+#    pprint.pprint(stmt)
 
 
 # Read XML file
 #print(f.read())
-
-obj = untangle.parse(track)
