6f5a985680b34ce977a4081fa0350b895e92d947
David Southwick <david.southwick@cern.ch>
2019-11-11 13:31:52 +0100

    working api query and bounding box for GPX parsing

--- convert.py
+++ convert.py
@@ -1,23 +1,26 @@
-
-import rdflib
+#!/usr/bin/env python3
 from rdflib import Graph, Namespace, RDF, Literal, BNode, RDFS, XSD, URIRef
 #import sparqlwrapper
 import gpxpy
 import gpxpy.gpx
 import requests
 import json
+import overpy
+# https://python-overpy.readthedocs.io/en/latest/example.html
+########################
+#
+# Usage: python3 convert.py
+# NB: requires course .gpx files in ./GPX_Tracks/*
+########################
 
-overpass_url = "http://overpass-api.de/api/interpreter"
-osm_query="https://api.openstreetmap.org/api/0.6/map?bbox=11.54,48.14,11.543,48.145"
-overpass_query = """
-[out:json];
-node ["amenity"="fuel
-(21.215460, -157.956619, 21.385951, -157.724533);
-out;
-"""
-response = requests.get(overpass_url,
-                        params={'data': overpass_query})
-#data = response.json()
+
+
+def query_OSM(left, bottom, right, top):
+    """ Query OSM api using box defined by edge long & lat """
+    api = overpy.Overpass()
+    result = api.query("node({},{},{},{});out;".format(left, bottom, right, top))
+    print("Returned {} nodes, {} ways, and {} relations from OSM".format(len(result.nodes), len(result.ways), len(result.relations)))
+    #data = response.json()
 
 # https://pypi.org/project/gpxpy/
 # pip3 install gpxpy rdflib --user
@@ -28,7 +31,9 @@ Track = URIRef("https://www.w3.org/2003/01/geo/wgs84_pos/")
 
 graph = Graph()
 
+
 def parse_gpx(gpx_file):
+    """ Read in GPX xml to rdflib Graph, and export turtle """
     left_bound = 100
     bottom_bound = 100
     right_bound = 0
@@ -38,16 +43,16 @@ def parse_gpx(gpx_file):
         for segment in track.segments:
                 geoNumber = 0
                 for point in segment.points:
-                    print('Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation))
+                    #print('Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation))
                     # get bounding area from track
-                    if point.longitude < left_bound:
-                        left_bound=point.longitude
-                    if point.latitude < bottom_bound:
-                        bottom_bound = point.latitude
-                    if point.longitude > right_bound:
-                        right_bound = point.longitude
-                    if point.latitude > top_bound:
-                        top_bound = point.latitude
+                    if point.latitude < left_bound:
+                        left_bound=point.latitude
+                    if point.longitude < bottom_bound:
+                        bottom_bound = point.longitude
+                    if point.latitude > right_bound:
+                        right_bound = point.latitude
+                    if point.longitude > top_bound:
+                        top_bound = point.longitude
 
                     node=BNode()
                     geo.lat=point.latitude
@@ -60,16 +65,19 @@ def parse_gpx(gpx_file):
 
                     geoNumber += 1
                 print("Created {0} points in RDF".format(geoNumber))
-                print("Found area bounding box of {},{},{},{}".format(left_bound, bottom_bound, right_bound, top_bound))
-    for waypoint in gpx.waypoints:
-        print('waypoint {0} -> ({1},{2})'.format(waypoint.name, waypoint.latitude, waypoint.longitude))
+                print("Found area bounding box of {}, {}, {}, {}".format(left_bound, bottom_bound, right_bound, top_bound))
+    #for waypoint in gpx.waypoints:
+        #print('waypoint {0} -> ({1},{2})'.format(waypoint.name, waypoint.latitude, waypoint.longitude))
 
-    for route in gpx.routes:
-        print('Route:')
-        for point in route.points:
-            print('Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation))
+    #for route in gpx.routes:
+        #print('Route:')
+        #for point in route.points:
+            #print('Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation))
+    return left_bound, bottom_bound, right_bound, top_bound
 
-def printGraph():
+
+def exportGraph():
+    """ Save rdfLib generated graph as turtle format for graphDB """
     #print(graph.serialize(format='turtle'))
     graph.serialize(destination="./graph.ttl", format='turtle')
 
@@ -89,9 +97,9 @@ def printGraph():
 #print(f.read())
 def main():
     gpx_file = open('GPX_Tracks/4sDDFdd4cjA.gpx', 'r')
-    print("python main function")
-    parse_gpx(gpx_file)
-    printGraph()
+    left, bottom, right, top = parse_gpx(gpx_file)
+    #exportGraph()
+    query_OSM(left, bottom, right, top)
 
 if __name__ == '__main__':
     main()
