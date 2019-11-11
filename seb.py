9e68cb6f1231f7b45f72fb3a0400053163d4772d
David Southwick <dsouthwi@iPhonedphilippe.home>
2019-11-11 16:13:02 +0100

    add geo example

--- seb.py
+++ seb.py
@@ -4,14 +4,17 @@ from rdflib.namespace import RDF, FOAF, RDFS, XSD
 from rdflib import URIRef, BNode, Literal, Graph, Namespace
 
 n = Namespace("http://cui.unige.ch/tws/tp1/")
+geo = Namespace("http://www.w3.org/2003/01/geo/wgs84_pos#")
 g = Graph()   # ttl output graph
 Track = URIRef("http://cui.unige/tws/tp1/Track")
 Natural = URIRef("http://cui.unige/tws/tp1/Natural")
 g.add((n.Track, RDFS.Class, RDFS.Resource))
 g.add((n.Track, RDF.type, RDFS.Class))
+g.add((n.Track, RDFS.range, geo.Point))
+g.add((geo.Point, ))
 
-g.serialize(destination="./file.ttl", format='turtle')
-
+#g.serialize(destination="./file.ttl", format='turtle')
+print(g.serialize(format='turtle').decode('UTF-8'))
 
 #It shall generate a rdf file like that
 
