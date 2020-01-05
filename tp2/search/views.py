821a5ab551f2b33948ec16af59292fcef6776fc4
hcalgit <hcal.git@cern.ch>
2020-01-05 03:27:34 +0100

    RDF frontend, better tables

--- tp2/search/views.py
+++ tp2/search/views.py
@@ -1,19 +1,36 @@
+# -*- coding: utf-8 -*-
 from django.views.generic import TemplateView
 from SPARQLWrapper import JSON, SPARQLWrapper2
+from requests import Timeout
+import django_tables2 as tables
+
+sparql = SPARQLWrapper2("http://192.168.1.232:7200/repositories/swoogle")
+sparql.setTimeout(2)
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
 class SearchResultsView(TemplateView):
     template_name = 'search_results.html'
-   
+    
     def query(self):
         return self.request.GET.get('q', '')
     
     def query_result(self):
-        q = self.request.GET.get('q', '')
-        sparql = SPARQLWrapper2("http://192.168.1.232:7200/repositories/swoogle")
-        sparql.setQuery(q)
-        ret = sparql.query()
-
-        return ret
+        query = self.request.GET.get('q', '')
+        sparql.setQuery(query)
+    
+        try:
+            response = sparql.query()
+        except TimeoutError:
+            return -1
+        except Exception as e:
+            print(e)
+            return -1
+        attrs = {}
+        for k in response.variables:
+            attrs[str(k)] = tables.Column()
+        responseTable = type('responseTable', (tables.Table,), attrs)
+        DJANGO_TABLES2_TEMPLATE = "django_tables2/bootstrap4.html"
+        return responseTable(response.bindings)
+        
--- tp2/test-rdfwrapper.py
+++ tp2/test-rdfwrapper.py
@@ -1,13 +1,17 @@
 from SPARQLWrapper import JSON, SPARQLWrapper2
-queryString = "SELECT * WHERE { ?s rdf:type ?o. } limit 10"
+queryString = "SELECT * WHERE { ?s rdf:type ?o. } limit 2"
 # get sparql endpoint from graphDB/setup/repositories link icon
 sparql = SPARQLWrapper2("http://192.168.1.232:7200/repositories/swoogle")
 
 sparql.setQuery(queryString)
 ret = sparql.query()
+print(type(ret))
 print(ret.variables)  
+print(ret.fullResult)
+print()
+print(ret.bindings)
 # this is an array consisting of "subj" and "prop"
-for binding in ret.bindings :
+#for binding in ret.bindings :
     # each binding is a dictionary. Let us just print the results
-    print("{}: {} (of type {})".format("s",binding[u"s"].value,binding[u"s"].type))
-    print("{}: {} (of type {})".format("o",binding[u"o"].value,binding[u"o"].type))
+#    print("{}: {} (of type {})".format("s",binding[u"s"].value,binding[u"s"].type))
+#    print("{}: {} (of type {})".format("o",binding[u"o"].value,binding[u"o"].type))
